72287f93817c231b4ffb37edd877fa44eabb0543
Arunkumar <arunkumar041093@gmail.com>
2024-02-05 11:45:59 +1300

    changed to jsx file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,12 +6,34 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] showdow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly flex.col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
 const About = () => {
   return (
     <>
       <motion.div variants={textVariant}>
         <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview</h2>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
       <motion.p
@@ -25,7 +47,11 @@ const About = () => {
         real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
 
-      <div className="mt-20 flex flex-wrap "></div>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   );
 };
